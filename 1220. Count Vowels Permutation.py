1d8196afbae64b3a6a075039c7128a70af194874
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-07 21:03:09 +0300

    Problem done

---
+++ 1220. Count Vowels Permutation.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
+    """
+    MOD = 10 ** 9 + 7
+
+    def countVowelPermutation(self, n: int) -> int:
+        a, e, i, o, u = 1, 1, 1, 1, 1
+
+        for _ in range(n - 1):
+            a, e, i, o, u = e + i + u, a + i, e + o, i, i + o
+
+        return (a + e + i + o + u) % self.MOD
