d30597f4b4c0203cd91280988a37f5e75418b26f
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-11 09:53:38 +0300

    Problem done

---
+++ 98. Validate Binary Search Tree.py
@@ -0,0 +1,30 @@
+from sys import maxsize
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(log(n))
+    """
+
+    INF = maxsize
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self._validate(root, -self.INF, self.INF)
+
+    @classmethod
+    def _validate(cls, root: Optional[TreeNode], min_val: int, max_val: int) -> bool:
+        if root is None:
+            return True
+        if root.val <= min_val or max_val <= root.val:
+            return False
+        return cls._validate( root.left,  min_val, root.val) and \
+               cls._validate(root.right, root.val,  max_val)
