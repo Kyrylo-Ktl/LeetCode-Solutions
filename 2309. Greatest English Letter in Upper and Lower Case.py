2ed51bbb32a3804d8ef494fa4d3f39401b80e1f7
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-14 00:15:58 +0300

    Problem done

---
+++ 2309. Greatest English Letter in Upper and Lower Case.py
@@ -0,0 +1,31 @@
+class Solution:
+    """
+    Time:   O(2*26*log(2*26))
+    Memory: O(2*26)
+    """
+
+    def greatestLetter(self, s: str) -> str:
+        letters = set(s)
+
+        for ltr in sorted(letters, reverse=True):
+            if ltr.isupper() and ltr.lower() in letters:
+                return ltr
+
+        return ''
+
+
+class Solution:
+    """
+    Time:   O(2*26)
+    Memory: O(2*26)
+    """
+
+    def greatestLetter(self, s: str) -> str:
+        letters = set(s)
+        greatest = ''
+
+        for ltr in letters:
+            if ltr.isupper() and ltr.lower() in letters:
+                greatest = max(ltr, greatest)
+
+        return greatest
