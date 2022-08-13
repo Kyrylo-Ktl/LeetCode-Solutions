86cdd31f4287a09004c717bd7f2cc11298a8ac1d
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-13 23:35:21 +0300

    Problem done

---
+++ 2363. Merge Similar Items.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    """
+    Time:   O((n+m)*log(n+m))
+    Memory: O(n+m)
+    """
+
+    def mergeSimilarItems(self, first: List[List[int]], second: List[List[int]]) -> List[List[int]]:
+        merged = defaultdict(int)
+
+        for value, weight in first + second:
+            merged[value] += weight
+
+        return sorted(merged.items())
