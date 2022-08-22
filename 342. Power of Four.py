57dfc85c7544046e1c9a9ccc76401c2c246506e8
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-22 16:03:02 +0300

    Problem done

--- 342. Power of Four.py
+++ 342. Power of Four.py
@@ -9,3 +9,15 @@ class Solution:
 
     def isPowerOfFour(self, n: int) -> bool:
         return n > 0 and log(n, 4).is_integer()
+
+
+class Solution:
+    """
+    Time:   O(1)
+    Memory: O(1)
+    """
+
+    POWERS = {4 ** n for n in range(16)}
+
+    def isPowerOfFour(self, n: int) -> bool:
+        return n in self.POWERS
