65dcff40b3c3063d6204cca7ef296c88802113bc
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-23 20:50:04 +0300

    Problem done

--- 895. Maximum Frequency Stack.py
+++ 895. Maximum Frequency Stack.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from heapq import heappop, heappush
 
 
 class FreqStack:
@@ -31,4 +32,33 @@ class FreqStack:
         if not self.stack[self.max_freq]:
             self.max_freq -= 1
 
+        return val
+
+
+class FreqStack:
+    """
+    Implementation of a frequency stack based on a heap
+
+    Memory:
+        creation - O(n)
+        'push' and 'pop' - O(1)
+
+    Time:
+        creation - O(n)
+        'push' and 'pop' - O(log(n))
+    """
+
+    def __init__(self):
+        self.frequency = defaultdict(int)
+        self.heap = []
+        self.index = 0
+
+    def push(self, val: int) -> None:
+        self.frequency[val] += 1
+        self.index += 1
+        heappush(self.heap, (-self.frequency[val], -self.index, val))
+
+    def pop(self) -> int:
+        _, _, val = heappop(self.heap)
+        self.frequency[val] -= 1
         return val
