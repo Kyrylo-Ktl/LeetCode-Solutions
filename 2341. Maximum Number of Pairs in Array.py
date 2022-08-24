d504011ec2f7545a08c2542319ee67e63ed44146
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-24 13:08:26 +0300

    Problem done

--- 2341. Maximum Number of Pairs in Array.py
+++ 2341. Maximum Number of Pairs in Array.py
@@ -11,3 +11,23 @@ class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
         pairs = sum(cnt // 2 for cnt in Counter(nums).values())
         return [pairs, len(nums) - 2 * pairs]
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(n)
+    """
+
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+        pairs = 0
+        single = set()
+
+        for num in nums:
+            if num in single:
+                single.remove(num)
+                pairs += 1
+            else:
+                single.add(num)
+
+        return [pairs, len(single)]
