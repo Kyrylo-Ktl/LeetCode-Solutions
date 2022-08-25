724fd71947b5b291635cf6097093e7a4f4b6e6e6
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-25 10:56:40 +0300

    Problem done

---
+++ 383. Ransom Note.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution:
+    """
+    Time:   O(n + m)
+    Memory: O(n + m)
+    """
+
+    def canConstruct(self, ransom_note: str, magazine: str) -> bool:
+        return not Counter(ransom_note) - Counter(magazine)
