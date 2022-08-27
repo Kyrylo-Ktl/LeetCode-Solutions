9f9fb11a83bac1f3eb1812a1e3d58ab27add3568
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-27 22:47:59 +0300

    Problem done

---
+++ 279. Perfect Squares.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from math import isqrt
+
+
+class Solution:
+    """
+    Time:   O(n*√n)
+    Memory: O(log(n))
+    """
+
+    def numSquares(self, n: int) -> int:
+        return self._decompose(n)
+
+    @classmethod
+    @lru_cache(None)
+    def _decompose(cls, n: int) -> int:
+        if n < 2:
+            return n
+        return 1 + min(cls._decompose(n - i * i) for i in range(1, isqrt(n) + 1))
