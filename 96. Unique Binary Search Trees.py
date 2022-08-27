af1d3f94235e933862c4782ab3230e40696390d9
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-27 16:56:14 +0300

    Problem done

--- 96. Unique Binary Search Trees.py
+++ 96. Unique Binary Search Trees.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from functools import reduce
 
 
 class Solution:
@@ -16,3 +17,13 @@ class Solution:
         if hi - lo < 1:
             return 1
         return sum(cls._num_trees(lo, i - 1) * cls._num_trees(i + 1, hi) for i in range(lo, hi + 1))
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
+    """
+
+    def numTrees(self, n: int) -> int:
+        return reduce(lambda res, i: res * 2 * (2 * i + 1) // (i + 2), range(n), 1)
