00d6be4d911eae34f9616464823ed1f47c985635
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-29 10:18:48 +0300

    Problem done

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -9,7 +9,6 @@ class Solution:
 
     LAND = '1'
     WATER = '0'
-    MOVES = ((1, 0), (-1, 0), (0, 1), (0, -1))
 
     def numIslands(self, grid: List[List[str]]) -> int:
         n, m = len(grid), len(grid[0])
