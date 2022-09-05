fcd8a87515f370cafd3f63931d5dd82d7171a55a
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-05 11:03:22 +0300

    Problem done

--- 223. Rectangle Area.py
+++ 223. Rectangle Area.py
@@ -4,6 +4,7 @@ class Solution:
     Memory: O(1)
     """
 
-    def computeArea(self, x1: int, y1: int, x2: int, y2: int, x3: int, y3: int, x4: int, y4: int) -> int:
+    def computeArea(self, *coords) -> int:
+        x1, y1, x2, y2, x3, y3, x4, y4 = coords
         overlap = max(min(x2, x4) - max(x1, x3), 0) * max(min(y2, y4) - max(y1, y3), 0)
         return (x2 - x1) * (y2 - y1) + (x4 - x3) * (y4 - y3) - overlap
