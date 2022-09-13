268d83a460a852079c14368c39c1d469ab0888ce
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-13 21:44:15 +0300

    Added handling '/' character in title

--- infrastructure/src/collector.py
+++ infrastructure/src/collector.py
@@ -25,7 +25,7 @@ class SolutionsCollector:
                 if file.startswith(IGNORE):
                     continue
                 if file.endswith(extension):
-                    solutions.add(file.removesuffix(extension))
+                    solutions.add(file.removesuffix(extension).replace('|', '/'))
                 else:
                     logger.warning(f'File with unknown extension for {language}: {file}')
 
