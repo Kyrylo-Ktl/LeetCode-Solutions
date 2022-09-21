22fb80736dfa02ad6105b9061451a614414dae84
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-21 20:31:56 +0300

    Added Python solution for "String Matching in an Array"

---
+++ Python/String Matching in an Array.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time:   O(n^2*k^2)
+    Memory: O(1)
+
+    where k - longest word length
+    """
+
+    def stringMatching(self, words: List[str]) -> List[str]:
+        words = sorted(words, key=len)
+        result = []
+
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if words[i] in words[j]:
+                    result.append(words[i])
+                    break
+
+        return result
