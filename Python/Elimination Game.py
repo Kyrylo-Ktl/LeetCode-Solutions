133cc30a7bf144f918f7699ae62017bb9477d10d
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-23 23:44:13 +0300

    Added Python solution for "Elimination Game"

---
+++ Python/Elimination Game.py
@@ -0,0 +1,54 @@
+# class Solution:
+#     """
+#     Time:   O(log(n))
+#     Memory: O(log(n))
+#
+#     Explanation:
+#     1. Initially we start elimination from left to right:
+#         f(1,2,3 ... n) = f(n | left)
+#     2. At each step, we eliminate odd numbers and start on the other side:
+#         f(n | left) = f(2,4,6 ... n | right)
+#         f(2,4,6 ... n | right) = 2*f(1,2,3 ... n/2 | right) = 2*f(n/2 | right)
+#     3. Let`s transform f(n | left) as follows:
+#         f(1,2,3 ... n | left) = ANS
+#         f(n+1-1,n+1-2,n+1-3 ... n+1-n | left) = n+1-ANS
+#         f(n,n-1,n-2 ... 1 | left) = f(n | right) = n+1-ANS
+#         f(n | right) = n+1-f(1,2,3 ... n | left)
+#     4. Based on the previous:
+#         f(n | left) = f(n/2 | right) = n/2+1-f(1,2,3 ... n/2 | left)
+#     5. As a result, we obtain the recursive relation:
+#         f(n) = 2*(n/2+1-f(n/2))
+#     """
+#
+#     def lastRemaining(self, n: int) -> int:
+#         if n == 1:
+#             return 1
+#         return 2 * (n // 2 + 1 - self.lastRemaining(n // 2))
+
+class Solution:
+    """
+    Time:   O(log(n))
+    Memory: O(1)
+
+    Explanation:
+    1. Initially we start elimination from left to right:
+        f(1,2,3 ... n) = f(n | left)
+    2. At each step, we eliminate odd numbers and start on the other side:
+        f(n | left) = f(2,4,6 ... n | right)
+        f(2,4,6 ... n | right) = 2*f(1,2,3 ... n/2 | right) = 2*f(n/2 | right)
+    3. Let`s transform f(n | left) as follows:
+        f(1,2,3 ... n | left) = ANS
+        f(n+1-1,n+1-2,n+1-3 ... n+1-n | left) = n+1-ANS
+        f(n,n-1,n-2 ... 1 | left) = f(n | right) = n+1-ANS
+        f(n | right) = n+1-f(1,2,3 ... n | left)
+    4. Based on the previous:
+        f(n | left) = f(n/2 | right) = n/2+1-f(1,2,3 ... n/2 | left)
+    5. As a result, we obtain the recursive relation:
+        f(n) = 2*(n/2+1-f(n/2))
+    """
+
+    def lastRemaining(self, n: int) -> int:
+        n = 1
+        while 2*n != n - n&1:
+            n = n + 1
+        return n
