f98877a0ff384bd5b55f2b00e31c278818da444d
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-23 17:17:26 +0300

    Remove tables to separated .txt files

--- infrastructure/src/formatter.py
+++ infrastructure/src/formatter.py
@@ -11,12 +11,7 @@ logger = logging.getLogger(__name__)
 
 class MarkdownFormatter:
     @classmethod
-    def format(cls):
-        with open(README_PATH, 'wt') as readme:
-            readme.write(cls.format_markdown())
-
-    @classmethod
-    def format_markdown(cls) -> str:
+    def format(cls) -> str:
         solutions = SolutionsCollector.collect_solutions()
         problems = SolutionsCollector.collect_problems(solutions)
         solutions_table = MarkdownTable.format(map(cls._format_problem, problems))
