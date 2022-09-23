9acd3b9e48ecaa8bea8c917da30a431e3d5df392
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-23 15:29:17 +0300

    Added complexity notations table n README

--- infrastructure/src/resources/template.py
+++ infrastructure/src/resources/template.py
@@ -5,6 +5,14 @@ The repository contains the best versions of my solutions to LeetCode problems
 
 ## Complexity analysis
 
+| Notation |     Name     |  Sign  |          Meaning           |
+|:--------:|:------------:|:------:|:--------------------------:|
+|   $o$    |   Little O   |  $<$   |  Less than, not equal to   |
+|   $O$    |    Big O     | $\leq$ |         Less than          |
+| $\Theta$ |    Theta     |  $=$   |          Equal to          |
+| $\Omega$ |  Big Omega   | $\geq$ |        Greater than        |
+| $\omega$ | Little Omega |  $>$   | Greater than, not equal to |
+
 <p align="center">
   <img alt="complexity_chart" src="https://user-images.githubusercontent.com/93226646/191949453-8b4a432e-a24d-4f23-813b-fc658d2b4e1e.png">
 </p>
