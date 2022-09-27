8499eaef893477d29d02fcf2c7a2c3465dcf9850
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-27 11:25:11 +0300

    Added Python solution for "Valid Palindrome"

---
+++ Python/Valid Palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
+    """
+
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+
+        return True
