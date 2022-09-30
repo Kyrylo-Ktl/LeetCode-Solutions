5319b84b55be695a265dd43682daf23d11905967
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-09-30 19:48:15 +0300

    Added Java solution for "Rotate Array"

---
+++ Java/Rotate Array.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    /**
+     * Time:   O(n)
+     * Memory: O(1)
+     */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+}
