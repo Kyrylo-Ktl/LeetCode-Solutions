9e0822a256ae9c4414a35e36a53318550eb0a4be
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-13 12:30:14 +0300

    Added Java solution for "Percentage of Letter in String"

--- Java/Percentage of Letter in String.java
+++ Java/Percentage of Letter in String.java
@@ -5,6 +5,24 @@ public class Solution {
      * Memory: O(1)
      */
     public int percentageLetter(String s, char letter) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); ++i)
+            if (s.charAt(i) == letter)
+                count += 1;
+
+        return count * 100 / s.length();
+    }
+}
+
+
+public class Solution {
+
+    /**
+     * Time:   O(n)
+     * Memory: O(1)
+     */
+    public int percentageLetter(String s, char letter) {
         return (int) (s.chars().filter(ch -> ch == letter).count() * 100 / s.length());
     }
 }
