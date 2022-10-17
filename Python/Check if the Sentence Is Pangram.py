c5fc97545232f06ec7d6fb8fa03901b5c319e34c
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-17 08:36:01 +0300

    Added Python solution for "Check if the Sentence Is Pangram"

--- Python/Check if the Sentence Is Pangram.py
+++ Python/Check if the Sentence Is Pangram.py
@@ -1,8 +1,58 @@
+from string import ascii_lowercase
+
+
 class Solution:
     """
     Time:   O(n)
-    Memory: O(n)
+    Memory: O(1)
+    """
+
+    def checkIfPangram(self, sentence: str) -> bool:
+        for c in ascii_lowercase:
+            if c not in sentence:
+                return False
+        return True
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
     """
 
     def checkIfPangram(self, sentence: str) -> bool:
         return len(set(sentence)) == 26
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
+    """
+
+    def checkIfPangram(self, sentence: str) -> bool:
+        seen = [False] * 26
+
+        for c in sentence:
+            seen[ord(c) - ord('a')] = True
+
+        for status in seen:
+            if not status:
+                return False
+
+        return True
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(1)
+    """
+
+    def checkIfPangram(self, sentence: str) -> bool:
+        seen = 0
+
+        for c in sentence:
+            seen |= 1 << ord(c) - ord('a')
+
+        return seen == (1 << 26) - 1
