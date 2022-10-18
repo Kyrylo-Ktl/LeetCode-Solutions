dbb2b1e6a9319be5424249e56f5507bd201e8a74
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-18 22:15:07 +0300

    Added Java solution for "Convert 1D Array Into 2D Array"

---
+++ Java/Convert 1D Array Into 2D Array.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+    /**
+     * Time:   O(n*m)
+     * Memory: O(1)
+     */
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n)
+            return new int[0][0];
+
+        int[][] result = new int[m][n];
+        for (int i = 0; i < original.length; i++)
+            result[i / n][i % n] = original[i];
+
+        return result;
+    }
+}
