a1322dbe33a477539a7a1fe07428117fd8b4f930
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-19 09:52:38 +0300

    Added Python solution for "Range Addition II"

--- Python/Range Addition II.py
+++ Python/Range Addition II.py
@@ -4,6 +4,20 @@ from typing import List
 class Solution:
     """
     Time:   O(n)
+    Memory: O(n)
+    """
+
+    def maxCount(self, rows: int, cols: int, ops: List[List[int]]) -> int:
+        if not ops:
+            return rows * cols
+
+        x, y = zip(*ops)
+        return min(x) * min(y)
+
+
+class Solution:
+    """
+    Time:   O(n)
     Memory: O(1)
     """
 
