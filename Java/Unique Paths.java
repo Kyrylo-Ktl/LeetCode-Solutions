21879035841524021309fe6b4eb8b33ce2c3e629
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-20 18:06:56 +0300

    Added Java solution for "Unique Paths"

---
+++ Java/Unique Paths.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    /**
+     * Time:   O(n*m)
+     * Memory: O(n*m)
+     */
+    public int uniquePaths(int n, int m) {
+        int[][] grid = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = grid[i][j - 1] + grid[i - 1][j];
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+}
