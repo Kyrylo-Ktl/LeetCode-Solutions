e1dea8f5250d8c9d9430989e1a47f16ab48e786c
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-25 11:16:27 +0300

    Added Java solution for "Check if the Sentence Is Pangram"

--- Java/Check if the Sentence Is Pangram.java
+++ Java/Check if the Sentence Is Pangram.java
@@ -21,6 +21,28 @@ public class Solution {
      * Memory: O(1)
      */
     public boolean checkIfPangram(String sentence) {
+        int[] powers = new int[27];
+        powers[0] = 1;
+
+        for (int i = 1; i < powers.length; ++i)
+            powers[i] = powers[i - 1] << 1;
+
+        int seen = 0;
+
+        for (char ch : sentence.toCharArray())
+            seen |= powers[ch - 'a'];
+
+        return seen == powers[26] - 1;
+    }
+}
+
+public class Solution {
+
+    /**
+     * Time:   O(n)
+     * Memory: O(1)
+     */
+    public boolean checkIfPangram(String sentence) {
         return sentence.chars().reduce(0, (seen, ch) -> seen | 1 << (ch - 'a')) == (1 << 26) - 1;
     }
 }
