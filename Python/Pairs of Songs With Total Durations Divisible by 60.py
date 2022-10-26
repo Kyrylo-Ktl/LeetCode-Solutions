0922899b4cc4625264170886dd510732ba8f2b77
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-10-26 14:16:02 +0300

    Added Python solution for "Pairs of Songs With Total Durations Divisible by 60"

--- Python/Pairs of Songs With Total Durations Divisible by 60.py
+++ Python/Pairs of Songs With Total Durations Divisible by 60.py
@@ -5,7 +5,7 @@ from typing import List
 class Solution:
     """
     Time:   O(n)
-    Memory: O(n)
+    Memory: O(1)
     """
 
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
@@ -13,7 +13,7 @@ class Solution:
         count = 0
 
         for t in time:
-            count += memo[(60 - t) % 60]
+            count += memo[-t % 60]
             memo[t % 60] += 1
 
         return count
