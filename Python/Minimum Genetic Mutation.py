5d4570012c85afc8c349fe5859e851b429870189
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-11-02 23:50:16 +0200

    Added Python solution description for "Minimum Genetic Mutation"

---
+++ Python/Minimum Genetic Mutation.py
@@ -0,0 +1,34 @@
+from collections import deque
+from typing import List, Generator
+
+
+class Solution:
+    """
+    Time:   O(n*m^2)
+    Memory: O(n)
+
+    where m - gen length
+    """
+
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        queue = deque([(start, 0)])
+        seen = {start}
+
+        while queue:
+            curr, steps = queue.popleft()
+
+            if curr == end:
+                return steps
+
+            for neighbor in self.get_mutations(curr):
+                if neighbor in bank and neighbor not in seen:
+                    queue.append((neighbor, steps + 1))
+                    seen.add(neighbor)
+
+        return -1
+
+    @staticmethod
+    def get_mutations(gen: str) -> Generator:
+        for i in range(len(gen)):
+            for c in 'ACGT':
+                yield gen[:i] + c + gen[i + 1:]
