c4aacb0ee42a5be8db49200c1097e7a899a50d73
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-12-07 08:32:13 +0200

    Added BFS Python solution for "Range Sum of BST"

--- Python/Range Sum of BST.py
+++ Python/Range Sum of BST.py
@@ -17,6 +17,30 @@ class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if root is None:
             return 0
-        return self.rangeSumBST(root.left, low, high) + \
+        return self.rangeSumBST(root.left,  low, high) + \
                self.rangeSumBST(root.right, low, high) + \
                root.val * (low <= root.val <= high)
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(n)
+    """
+
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        queue = [root]
+        range_sum = 0
+
+        while queue:
+            node = queue.pop()
+            if low <= node.val <= high:
+                range_sum += node.val
+
+            if node.left is not None:
+                queue.append(node.left)
+
+            if node.right is not None:
+                queue.append(node.right)
+
+        return range_sum
