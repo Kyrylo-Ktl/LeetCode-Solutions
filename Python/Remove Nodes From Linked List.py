a9863fc49cb2f87f51d16b09a2f4146a8137cf07
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2023-03-22 19:17:30 +0200

    Added Python solution for "Remove Nodes From Linked List"

---
+++ Python/Remove Nodes From Linked List.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """
+    Time:   O(n)
+    Memory: O(n)
+    """
+
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        head.next = self.removeNodes(head.next)
+
+        if head.next is not None and head.val < head.next.val:
+            head = head.next
+
+        return head
